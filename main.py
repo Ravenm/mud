7fabd04e8b44b4e6c2759b6b871cc3fc50c8fa3e
Ravenm <ravenmetals@gmail.com>
2015-05-23 12:11:49 -0500

    this is just a test

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def template_test():
+    return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run()
