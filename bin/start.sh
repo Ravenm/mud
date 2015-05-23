840c813418c74bd66aab224991a2b183176109d0
frikfry <frikfry@gmail.com>
2015-05-23 13:39:02 -0400

    Use absolute path

--- bin/start.sh
+++ bin/start.sh
@@ -1,10 +1,12 @@
-if [ -e pid.txt ]; then
-  kill -15 $(cat pid.txt) | true
-  rm pid.txt
+BASEDIR=$(dirname $0/..)
+
+if [ -e $BASEDIR/pid.txt ]; then
+  kill -15 $(cat $BASEDIR/pid.txt) | true
+  rm $BASEDIR/pid.txt
 fi
 
-pip install -r requirements.txt
+pip install -r $BASEDIR/requirements.txt
 
 export BUILD_ID=dontKillMe
-nohup python main.py &
-echo $! > pid.txt
+nohup python $BASEDIR/main.py &
+echo $! > $BASEDIR/pid.txt
