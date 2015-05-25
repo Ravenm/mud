a7829412e35fd1f87be99e711d2ebfd2c9d15e3d
Ravenm <ravenmetals@gmail.com>
2015-05-25 04:26:33 -0500

    background test

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def template_test():    
+def template_test():
     return render_template('template.html', motd="For help or commands type !help")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
