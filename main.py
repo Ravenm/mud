db97e8ef00be044febbd7c204eaf2837c375f9aa
FrikFry <frikfry@gmail.com>
2015-05-31 19:27:47 -0400

    SocketIO in progress

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 
 @app.route('/')
 def template_test():
     return render_template('template.html', motd="For help or commands type !help")
+
+
+@socketio.on("communication", namespace="/mud")
+def communicate(message):
+    print message
+    emit('communication', {"message": "Received"})
 
 
 if __name__ == '__main__':
