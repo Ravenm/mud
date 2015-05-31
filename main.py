397360895486dc3f282210898bb44dfb37d44603
FrikFry <frikfry@gmail.com>
2015-05-31 18:37:49 -0400

    Fixing a pep error

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def template_test():
     return render_template('template.html', motd="For help or commands type !help")
