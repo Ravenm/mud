be1a6c70f47cee92e992a3d56e587b10a7771870
FrikFry <frikfry@gmail.com>
2015-05-31 21:19:42 -0400

    Now we ask for and receive a username

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask.ext.socketio import SocketIO, emit
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = '\x8e \x99\xe2\xc0\xda\xe1\xcdQ\xcb$\x05\xcf\xca\x1e|\xdb\xfa\xb4\x1f\xbd\xbc&\xb2'
 socketio = SocketIO(app)
 
 
@@ -14,10 +14,34 @@ def template_test():
 
 @socketio.on("communication", namespace="/mud")
 def communicate(message):
-    if message["data"].startswith("!"):
-        print "Command!"
+    if 'logged' in session:
+        if message["data"].startswith("!"):
+            print "Command!"
+        else:
+            message["name"] = session["name"]
+            emit('communication', message, broadcast=True)
+    elif message["data"].startswith("!register"):
+        try:
+            name = message["data"].split()[1]
+        except IndexError:
+            server_communication("Please include a username with your request: !register USERNAME")
+            return
+        session["logged"] = True
+        session["name"] = name
+    elif message["data"].startswith("!login"):
+        try:
+            name = message["data"].split()[1]
+        except IndexError:
+            server_communication("Please include a username with your request: !login USERNAME")
+            return
+        session["logged"] = True
+        session["name"] = name
     else:
-        emit('communication', message, broadcast=True)
+        server_communication("Please login or register with !login USERNAME or !register USERNAME")
+
+
+def server_communication(msg, broadcast=False):
+    emit('communication', {"name": "Server", "data": msg}, broadcast=broadcast)
 
 
 if __name__ == '__main__':
