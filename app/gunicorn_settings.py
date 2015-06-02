fe0c063ca6d41f362b63d7ee1c6281d4342defc5
FrikFry <frikfry@gmail.com>
2015-06-02 19:00:41 -0400

    Added more settings

--- app/gunicorn_settings.py
+++ app/gunicorn_settings.py
@@ -3,3 +3,7 @@ import multiprocessing
 bind = "0.0.0.0:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "socketio.sgunicorn.GeventSocketIOWorker"
+pid = "../pid.txt"
+loglevel = "debug"
+accesslog = "-"
+errorlog = "-"
